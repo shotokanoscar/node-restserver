3ef84f748cfc4713b7cb66e474d72fc88bc0205a
Oscar <oscar.uribe2011@gmail.com>
2020-07-08 22:24:06 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://shotokan:wmSjCuSWtZ3BeXhA@cluster0.lqdnl.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
