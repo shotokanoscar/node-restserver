b901bcdee6c78c2ff062c29692359e91f141b134
Oscar <oscar.uribe2011@gmail.com>
2020-07-20 23:07:06 -0400

    Google Sing-in

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(bodyParser.json());
 // configuracion global de rutas 
 app.use(require('./routes/index'));
 
+// habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
